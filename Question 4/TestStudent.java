48ca3f2367c4e1247edb137b143c89071f0cc8c5
Aniket <aniketmondal37@gmail.com>
2025-01-22 20:27:47 +0530

    :heavy_plus_sign: Adding the logic of switch case

--- Question 4/TestStudent.java
+++ Question 4/TestStudent.java
@@ -1,29 +1,47 @@
+
 import java.util.*;
 
 public class TestStudent {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Student s[] = new Student[2];
-        
+        String swc;
         //Accepting the values
-        for(int i=0;i<s.length;i++){
+        for (int i = 0; i < s.length; i++) {
             s[i] = new Student();
-            System.out.println("Enter the details for Student "+(i+1));
+            System.out.println("Enter the details for Student " + (i + 1));
             System.out.println("--------------------");
             System.out.println("Enter Roll Number");
             s[i].setRoll(sc.nextInt());
             System.out.println("Enter Name");
             s[i].setName(sc.next());
-            System.out.println("Enter marks");
-            s[i].setMarks(sc.nextInt());
+            System.out.println("Enter the certification done of these following -");
+            System.out.println("1.OCJP\t2.OCA\t3.MCSA\t4.GCP");
+            swc = sc.next();
+            switch (swc) {
+                case "GCP":
+                    s[i].setMarks(92);
+                    break;
+                case "OCJP":
+                    s[i].setMarks(90);
+                    break;
+                case "OCA":
+                    s[i].setMarks(80);
+                    break;
+                case "MCSA":
+                    s[i].setMarks(77);
+                default:
+                    System.out.println("Invalid Input");
+            }
         }
 
         //Printing the values
-        for(Student i:s){
+        for (Student i : s) {
             System.out.println("Details of Student ");
-            System.out.println("Roll of the student "+i.getRoll());
-            System.out.println("Name of the student "+i.getName());
-            System.out.println("Marks of the student "+i.getMarks());
+            System.out.println("Roll of the student " + i.getRoll());
+            System.out.println("Name of the student " + i.getName());
+            System.out.println("Marks of the student " + i.getMarks());
             System.out.println("----------------------------");
         }
     }
